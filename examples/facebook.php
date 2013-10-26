957b710486642cdcf85b61bdda4bed7ee63c6c3c
Task <diving@task.co.id>
2013-10-26 14:48:51 +0700

    Create facebook.php

---
+++ examples/facebook.php
@@ -0,0 +1,7 @@
+require_once("facebook.php");
+
+$config = array();
+$config['appId'] = '1430449897176275';
+$config['secret'] = '7aab06f0a847215405292c13406580cc';
+
+$facebook = new Facebook($config);
